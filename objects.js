35991071034d655856b9fe3f47ceba5619c67308
Denise Pendleton <dpendl1@hotmail.com>
2018-01-07 19:43:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,10 @@
-var playlist = {artistName : "songTitle"}
+var playlist = {artistName : "songTitle"};
 
-function updatePlaylist(playlist,artistName, songTitle ){
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = 'Phil Ochs'
   playlist.songTitle ="Here's to the State of Mississippi"
+  
+  playlist[artistName] = songTitle;
 
-return playlist.artistName + playlist.songTitle
+  return playlist.artistName + playlist.songTitle
 }
