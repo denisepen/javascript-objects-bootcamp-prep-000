92f8365fd03c168afe2f58fe35a8bf2ff9c9c938
Denise Pendleton <dpendl1@hotmail.com>
2018-01-07 19:48:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-var playlist = {};
+var playlist = {artistName : "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   // playlist.artistName = 'Phil Ochs'
   // playlist.songTitle ="Here's to the State of Mississippi"
   
-  playlist[artistName] = songTitle;
+  // playlist[artistName] = songTitle;
+  playlist.artistName = songTitle;
 
   return playlist;
 }
